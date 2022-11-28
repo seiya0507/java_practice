cdd0f34d724234a619447cfe4575dcd741a6d1e7
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-11-28 13:57:51 +0000

    [Update] cloud9でjava勉強開始

---
+++ Variable01.java
@@ -0,0 +1,17 @@
+public class Variable01 {
+
+  public static void main(String[] args) {
+    int num1; // 宣言
+    String str1;
+    num1 = 10;
+    str1 = "Hello World!"; // 初期化
+    System.out.println(num1);
+    System.out.println(str1);
+
+    num1 = 20; // 代入
+    System.out.println(num1);
+
+    int num2 = 30; // 宣言+初期化
+    System.out.println(num2);
+  }
+}
