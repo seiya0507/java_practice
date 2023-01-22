eb3c12a20a6a576e0c0225e24df2fa2591cd8812
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2023-01-22 15:07:15 +0000

    [Update] カプセル化復習

--- NoCapsuleMan.java
+++ NoCapsuleMan.java
@@ -1,11 +1,11 @@
 package practice;
 
-public class NoCapsuleMan {
-    public String name;
-    public int age;
-
-    public NoCapsuleMan(String name, int age) {
-        this.name = name;
-        this.age = age;
+public class Chapter12 {
+    public static void main(String[] args) {
+        CapsuleMan man = new CapsuleMan("小林", 32);
+        System.out.println("名前は" + man.getName() + "です。");
+        System.out.println("年齢は" + man.getAge() + "です。");
+        man.setAge(33);
+        System.out.println("誕生日を迎えたので、年齢が" + man.getAge() + "になりました。");
     }
 }
