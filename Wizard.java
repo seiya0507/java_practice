1ca1647ea84011ecfa6eb20cb579d258dd018eaf
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2023-02-01 15:17:00 +0000

    [Update] 練習問題

--- Wizard.java
+++ Wizard.java
@@ -5,10 +5,18 @@ public class Wizard {
   private Wand wand;
   public void heal(Hero h) {
     int basePoint = 10;
-    int recovPoint = (int) (basePoint * this.wand.power);
-
+    int recovPoint =
+        (int) (basePoint * this.getWand().getPower());
     h.setHp(h.getHp() + recocPoint);
     System.out.println
         (h.getName() + "のHPを" + recovPoint + "回復した！");
   }
+  public int getHp() { return this.hp; }
+  public void setHp(int hp) { this.hp = hp; }
+  public int getMp() { return this.mp; }
+  public void setMp(int mp) { this.mp = mp; }
+  public String getName() { return this.name; }
+  public void setName(String name) { this.name = name; }
+  public Wand getWand() { return this.wand; }
+  public void setWand(Wand wand) { this.wand = wand; }
 }
