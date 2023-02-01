86f3d108b243696dcf9c3f3bee82d71eaeac46f8
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2023-02-01 15:00:47 +0000

    [Update] 練習問題

---
+++ Wizard.java
@@ -0,0 +1,14 @@
+public class Wizard {
+  int hp;
+  int mp;
+  String name;
+  Wand wand;
+  void heal(Hero h) {
+    int basePoint = 10;
+    int recovPoint = (int) (basePoint * this.wand.power);
+
+    h.setHp(h.getHp() + recocPoint);
+    System.out.println
+        (h.getName() + "のHPを" + recovPoint + "回復したい");
+  }
+}
