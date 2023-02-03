59282038fde2f56f18e7b36e7607e790d5d9830b
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2023-02-03 15:14:11 +0000

    [Update] 練習問題

--- Wizard.java
+++ Wizard.java
@@ -12,11 +12,32 @@ public class Wizard {
         (h.getName() + "のHPを" + recovPoint + "回復した！");
   }
   public int getHp() { return this.hp; }
-  public void setHp(int hp) { this.hp = hp; }
+  public void setHp(int hp) {
+    if (hp < 0) { this.hp = 0; } else { this.hp = hp; }
+
+  }
   public int getMp() { return this.mp; }
-  public void setMp(int mp) { this.mp = mp; }
+  public void setMp(int mp) {
+    if (mp < 0) {
+      throw new IllegalArgumentException
+          ( "設定されようとしているMPが異常です" );
+    }
+    this.mp = mp;
+  }
   public String getName() { return this.name; }
-  public void setName(String name) { this.name = name; }
+  public void setName(String name) {
+    if (name == null || name.length() < 3) {
+      throw new IllegalArgumentException
+          ( "魔法使いに設定されようとしている名前が異常です" );
+    }
+    this.name = name;
+  }
   public Wand getWand() { return this.wand; }
-  public void setWand(Wand wand) { this.wand = wand; }
+  public void setWand(Wand wand) {
+    if (wand == null) {
+      throw new IllegalArgumentException
+          ( "設定されようとしている杖がnullです" );
+    }
+    this.wand = wand;
+  }
 }
